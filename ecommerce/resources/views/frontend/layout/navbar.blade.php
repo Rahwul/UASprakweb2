553cb8159d754c2d1985d94f09857aadb6db290d
Rahwul <rahwul.ihsan20@gmail.com>
2023-07-10 18:46:09 -0700

    frontend navbar

--- ecommerce/resources/views/frontend/layout/navbar.blade.php
+++ ecommerce/resources/views/frontend/layout/navbar.blade.php
@@ -1,38 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Small Business - Start Bootstrap Template</title>
-        <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="{{ asset('frontend/assets/favicon.ico') }}" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="{{ asset('frontend/css/styles.css') }}" rel="stylesheet" />
-    </head>
-    <body>
-        <!-- Responsive navbar-->
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container px-5">
-                <a class="navbar-brand" href="#!">Start Bootstrap</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a class="nav-link" aria-current="page" href="{{ url('frontend/dashboard') }}">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="{{ url('frontend/about') }}">About</a></li>
-                        <li> <a class="nav-link" aria-current="page" href="{{ route('logout') }}"
-                            onclick="event.preventDefault();
-                                          document.getElementById('logout-form').submit();">
-                             {{ __('Logout') }}
-                         </a>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Small Business - Start Bootstrap Template</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="{{ asset('frontend/assets/favicon.ico') }}" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="{{ asset('frontend/css/styles.css') }}" rel="stylesheet" />
 
-                         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                             @csrf
-                         </form>
-                        </li>
+    <!-- Custom CSS -->
+    <style>
+        .navbar-logo {
+            font-size: 24px;
+            font-weight: bold;
+            color: #333;
+            /* Add custom styles for your logo */
+        }
 
-                    </ul>
-                </div>
+        .navbar-toggler-icon-custom {
+            /* Add custom styles for your toggle icon */
+        }
+
+        .navbar-nav .nav-link {
+            color: #333;
+            /* Add custom styles for your navbar links */
+        }
+
+        .navbar-nav .nav-link:hover {
+            color: #555;
+            /* Add custom styles for hover effect */
+        }
+    </style>
+</head>
+<body>
+    <!-- Responsive navbar-->
+    <nav class="navbar navbar-expand-md navbar-light bg-light">
+        <div class="container-fluid px-5">
+            <a class="navbar-logo" href="#!">Start Bootstrap</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon navbar-toggler-icon-custom"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item"><a class="nav-link" aria-current="page" href="{{ url('frontend/dashboard') }}">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ url('frontend/about') }}">About</a></li>
+                    <li>
+                        <a class="nav-link" aria-current="page" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </li>
+                </ul>
             </div>
-        </nav>
+        </div>
+    </nav>
+</body>
+</html>
